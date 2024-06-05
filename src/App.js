1a5ec1eaf567acc2698741a6b0ab6dc30c9cb806
Shaminilakkaraju <shaminilakkaraju@gmail.com>
2024-06-06 00:19:49 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const App = () => (
     <GlobalStyles />
     <Navbar />
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/gallery" element={<Gallery />} />
       <Route path="/testimonials" element={<Testimonials />} />
